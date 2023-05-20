e7b32f415ebdec06e6318f7a39155e13d8261c31
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-20 15:14:31 +0200

    unbloat key interpreter using trie

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,8 @@
 """
 keyboard input interpreter
+
+
+in all the program, <esc> is a global key binding to interrupt input sequences
 """
 
 
@@ -8,6 +11,8 @@ from typing import Callable
 
 # TODO: remove using this
 from ast import Note
+
+from lib import trie
 
 # TODO: replace by special entry in dictionnary,
 # for contextual sequence break behaviour override
@@ -22,6 +27,55 @@ from ast import Note
 # each time a new activity is created, the events are inherited (but the inputs are not automatically redirected)
 # other levels could be added, but the input receiver has to store all the event list
 # there is no "event propagation" as it would conflict when multiple break_sequence() events are raised
+
+
+class KeyInterpreter:
+
+    def __init__(self):
+        self.bindings = trie()
+        self.sequence = []
+
+    def define(self, seq, callback):
+        "simple ascii sequence -> callback"
+        self.bindings.set(seq, callback)
+
+    def execute(self, ch):
+        assert isinstance(ch, int)
+        # TODO: use an activity to display the ascii code for the escape char
+        # also check the get/set_escdelay(ms) in curses if escape is slow
+        """
+        if ch == KEY_ESC:
+            pass
+        else:
+        """
+        self.sequence.append(ch)
+        if (callback := self.bindings.get(self.sequence)) is not None:
+            callback()
+            self.sequence = []
+
+    # import bindings
+    def extend(self, registry: trie, head=None):
+        """
+        define() all the bindings existing in the registry
+        """
+        for key, value in registry.items():
+            self.define(key, value)
+
+    # unfortunately, i could not find a proper way of using decorators to dynamically bind instance methods to local instances of KeyInterpreter without abusing inheritance
+    # so you will have to define events with manual calls to on()
+    def on(self, callback, *args):
+        """
+        example:
+        kb.on(debug, KEY_DOWN, KEY_UP, KEY_ENTER, 'db')
+        """
+        sequence = []
+        for part in args:
+            if isinstance(part, int):
+                sequence.append(part)
+            else:
+                for ch in part:
+                    sequence.append(ord(ch))
+        self.define(sequence, callback)
 
 
 
