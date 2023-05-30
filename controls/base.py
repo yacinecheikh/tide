625202aca5b7386a856c5ad6e5b233348eda72da
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-05-30 06:15:49 +0200

    make Line a base graphic component

--- controls/base.py
+++ controls/base.py
@@ -4,6 +4,13 @@ multi-purpose primitives
 Nodes are used for DOM-like structured data
 Nodes should be subclassed to implement more behaviour, like syntax tree editing
 
+
+Lines represent mutable, multipart strings.
+The text can include blanks for transparency, and different styles for each part
+
+Texts are just renderable line lists.
+Texts can be used where Lines can.
+
 Texts are multipart areas that render (static) data.
 Texts can handle color and can be dynamically changed their data.
 Texts are currently very bloat, but they provide an api to render about anything.
@@ -59,58 +66,72 @@ class Node(Control):
         return width, height
 
 
+
+class Line(Control):
+    """
+    multipart single-line text
+
+    parts can be:
+    -str (default style)
+    -(str, 'default') for default (or line) style
+    -(None, length) for (transparent) blanks
+    -(text, style) for custom texts
+
+    to change parts, you can access and edit line.parts
+    """
+
+    # TODO: constructor
+    # 'text', ('text', styles.blink), 5
+    def __init__(self, *parts):
+        self.style = styles.default
+        self.parts = []
+        for elt in parts:
+            self.add(elt)
+
+    def add(self, element):
+        # auto-format str to (str, style)
+        if isinstance(element, str):
+            element = (element, 'default')
+        # item is (str, style) or (None, length)
+        self.contents.append(element)
+
+    def render(self, screen, x, y, default_style):
+        for elt in self.contents:
+            text, param = elt
+            if text is None:
+                # transparency (blank spaces)
+                x += param
+            else:
+                # override default style
+                if param == 'default':
+                    style = default_style
+                else:
+                    style = param
+                screen.write(x, y, text, style)
+                x += len(text)
+
+        # width
+        return x
+
+
 class Text(Control):
     """
     Kitchen sink, multipart text
 
-    style: common style for all text parts without a dedicated style (can be modified after creation)
+    style: default style for all text elements without a specified style (can be modified after creation)
     lines: split lines for quick block indenting and/or line iteration
-
-    Lines are renderable objects. Can contain subparts with a dedicated style, and use the parent text style as default
 
     this format allows both simple, multiline text
     and the usage of complex animated, non-ascii
     or transparent parts
     """
-
-
-    class Line:
-        def __init__(self, *parts):
-            #self.style = default_style
-            self.contents = []
-            for elt in parts:
-                self.add(elt)
-
-        def add(self, element):
-            # auto-format str to (str, style)
-            if isinstance(element, str):
-                element = (element, 'default')
-            # item is (str, style) or (None, length)
-            self.contents.append(element)
-
-        def render(self, screen, x, y, default_style):
-            for elt in self.contents:
-                text, param = elt
-                if text is None:
-                    # transparency (blank spaces)
-                    x += param
-                else:
-                    # override default style
-                    if param == 'default':
-                        style = default_style
-                    else:
-                        style = param
-                    screen.write(x, y, text, style)
-                    x += len(text)
-
-            # width
-            return x
-
     # TODO: constructor styles
     # Text()
     # Text(style = style)
     # Text(multiline)
     # Text((str1, style), (str2, style),...)
+    # Text([str, (str, style), str],
+    #      [], style=styles.default)  # 1 list = 1 line
 
 
     def __init__(self, text: str, style=None):
